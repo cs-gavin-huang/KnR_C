428fb34057ba4e13c142d2ff77f6966b1d2ff047
you-know-who-2017 <1376397133@qq.com>
2019-11-03 19:16:41 +0800

    update

---
+++ Chap1/1/hello.c
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: you-know-who-2017
+ * @Github: https://github.com/you-know-who-2017
+ * @Date: 2019-11-03 19:11:30
+ * @LastEditors: you-know-who-2017
+ * @LastEditTime: 2019-11-03 19:11:30
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("hello world\n");
+    return EXIT_SUCCESS;
+}
